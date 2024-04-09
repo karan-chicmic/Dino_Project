8b5f48bccaa890ab56bf8f3208f7ed39addcdfb4
Abhishek Rawat <abhishek.rawat@chicmic.co.in>
2024-04-09 11:31:03 +0530

    box bug pending

--- assets/script/customcomponent.ts
+++ assets/script/customcomponent.ts
@@ -16,7 +16,8 @@ import {
     Vec3,
     view,
 } from "cc";
-import { hurdle } from "./hurdle";
+import { hurdle } from "./hurdle"; // Assuming "hurdle" script defines cactus behavior
+
 const { ccclass, property } = _decorator;
 
 @ccclass("component")
@@ -26,82 +27,55 @@ export class component extends Component {
     @property({ type: Prefab })
     nodePrefab: Prefab = null;
     instanceNode: Node[] = [];
-    insNode;
+    gameOver: boolean = false; // Flag to track game state
+    insNode: Node;
+    isJumping = false;
 
     onLoad() {
         input.on(Input.EventType.TOUCH_START, this.onTouchStart, this);
 
-        // let newpos = this.getComponent(UITransform).convertToNodeSpaceAR(new Vec3(pos.x, pos.y, 0));
-
-        // this.dino.setPosition(newpos);
-        console.log("onLoad of customComponent");
         for (let i = 0; i < 5; i++) {
             this.insNode = instantiate(this.nodePrefab);
             this.node.addChild(this.insNode);
             this.instanceNode.push(this.insNode);
             this.insNode.getComponent(hurdle).setHurdle();
-            // console.log(this.instanceNode.position);
 
-            // let newpos = this.getComponent(UITransform).convertToWorldSpaceAR(
-            //     new Vec3(view.getDesignResolutionSize().x / 2, view.getDesignResolutionSize().y / 2, 0)
-            //     // instanceNode.position
-            // );
-        }
-        this.initPosition();
-    }
-    onTouchStart(event: EventTouch) {
-        // this.getComponent(UITransform).convertToNodeSpaceAR(
-        //     new Vec3(this.dino.getPosition().x, this.dino.getPosition().y, 0)
-        // );
-
-        tween(this.dino)
-            .to(
-                0.5,
-                {
-                    position: new Vec3(this.dino.getPosition().x + 10, this.dino.getPosition().y + 300, 0),
-                },
-                { easing: "cubicInOut" }
-            )
-            .to(
-                0.5,
-                {
-                    position: new Vec3(this.dino.getPosition().x + 20, this.dino.getPosition().y, 0),
-                },
-                { easing: "cubicInOut" }
-            )
-            .start();
-    }
-    initPosition() {
-        for (let i = 0; i < 5; i++) {
             this.instanceNode[i].position.set(
-                view.getDesignResolutionSize().x / 2 + i * 800,
+                view.getDesignResolutionSize().x / 2 + i * 950,
                 -view.getDesignResolutionSize().y / 2 +
                     this.instanceNode[i].getComponent(UITransform).contentSize.height,
                 0
             );
         }
     }
-    checkCollision(dino: Node, cactus: Node) {
-        let cactusx = cactus.position.x;
-        let cactusy = cactus.position.y;
-        let dinox = this.dino.position.x;
-        let dinoy = this.dino.position.y;
-        let dinoHeight = this.dino.position.y;
-        if (dinox >= cactusx + 150 || dinoy >= cactusy + 150) {
-            console.log("Game over!");
 
-            return;
+    onTouchStart(event: EventTouch) {
+        if (!this.isJumping) {
+            this.isJumping = true; // Set jumping flag to prevent further jumps
+
+            tween(this.dino)
+                .to(
+                    0.45,
+                    {
+                        position: new Vec3(this.dino.getPosition().x + 8, this.dino.getPosition().y + 500, 0),
+                    },
+                    { easing: "cubicInOut" }
+                )
+                .to(
+                    0.45,
+                    {
+                        position: new Vec3(this.dino.getPosition().x + 16, this.dino.getPosition().y, 0),
+                    },
+                    { easing: "cubicInOut", onComplete: () => (this.isJumping = false) } // Reset jump flag after completion
+                )
+                .start();
         }
-        // const rigidDino = dino.getComponent(RigidBody);
-        // rigidDino.useCCD = true;
-        // const rigidCactus = cactus.getComponent(RigidBody);
-        // rigidCactus.useCCD = true;
     }
 
     startMove() {
         for (let index = 0; index < this.instanceNode.length; index++) {
             const element = this.instanceNode[index];
-            element.setPosition(element.getPosition().x - 10, element.getPosition().y, 0);
+            element.setPosition(element.getPosition().x - 14, element.getPosition().y, 0);
             if (element.getPosition().x < -view.getDesignResolutionSize().x) {
                 element.setPosition(
                     view.getDesignResolutionSize().x * 1.3 + element.getComponent(UITransform).width + 350,
@@ -111,10 +85,22 @@ export class component extends Component {
             }
         }
     }
-    start() {}
+    checkCollision() {
+        for (const cactus of this.instanceNode) {
+            const dinoWorldBounds = this.dino.getComponent(UITransform).getBoundingBoxToWorld();
+            const cactusWorldBounds = cactus.getComponent(UITransform).getBoundingBoxToWorld();
+
+            if (cactusWorldBounds.intersects(dinoWorldBounds)) {
+                console.error("Game over!");
+                this.gameOver = true; // Ensure you've declared this property in your component
+
+                this.node.destroy();
+            }
+        }
+    }
 
     update(deltaTime: number) {
         this.startMove();
-        this.checkCollision(this.dino, this.insNode);
+        this.checkCollision();
     }
 }
