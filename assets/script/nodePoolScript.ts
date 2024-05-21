18abcc47772896eba96be9cc55ac92a118fae34a
Abhishek Rawat <abhishek.rawat@chicmic.co.in>
2024-05-21 19:29:42 +0530

    empty commit

---
+++ assets/script/nodePoolScript.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('nodePoolScript')
+export class nodePoolScript extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
